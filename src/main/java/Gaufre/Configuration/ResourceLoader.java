dfb30a82364b19601c1bea82ee0670c52a2f4b59
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2024-04-24 18:54:20 +0200

    better resource loading and error handling

--- src/main/java/Gaufre/Configuration/ResourceLoader.java
+++ src/main/java/Gaufre/Configuration/ResourceLoader.java
@@ -1,25 +1,58 @@
 package Gaufre.Configuration;
 
+import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import javax.imageio.ImageIO;
+
 public class ResourceLoader {
-    static public InputStream getResourceAsStream(String relativePath)
-            throws NullPointerException, FileNotFoundException {
-        InputStream resourceStream;
+    static public InputStream getResourceAsStream(String relativePath) {
+        InputStream resourceStream = null;
         if (Config.estJar()) {
             resourceStream = ResourceLoader.class.getClassLoader().getResourceAsStream(relativePath);
             if (resourceStream == null) {
-                throw new NullPointerException("Resource not found: " + relativePath);
+                System.err.println("Image " + relativePath + " not found.");
+                resourceStream = ResourceLoader.class.getClassLoader().getResourceAsStream("images/notFound.png");
+                if (resourceStream == null) {
+                    System.err.println("While handling missing resource " + relativePath + " new error occured :");
+                    System.err.println("Missing required resource images/notFound.png");
+                    System.exit(1);
+                }
             }
         } else {
-            resourceStream = new FileInputStream(relativePath);
-            // Jsp pk c'est flag comme dead code
-            if (resourceStream == null) {
-                throw new FileNotFoundException("File not found: " + relativePath);
+            try {
+                resourceStream = new FileInputStream(relativePath);
+            } catch (FileNotFoundException e1) {
+                System.err.println("Image " + relativePath + " not found.");
+                try {
+                    resourceStream = new FileInputStream("images/notFound.png");
+                } catch (FileNotFoundException e2) {
+                    System.err.println("While handling " + e1 + " new error occured :");
+                    System.err.println("Missing required file images/notFound.png : " + e2);
+                    System.exit(1);
+
+                }
             }
         }
         return resourceStream;
+    }
+
+    static public BufferedImage lireImage(String nom) {
+        String imgPath = "images/" + nom + ".png";
+        InputStream in;
+        try {
+            in = ResourceLoader.getResourceAsStream(imgPath);
+            return ImageIO.read(in);
+        } catch (FileNotFoundException e) {
+            System.err.println("Erreur: fichier " + imgPath + " introuvable");
+        } catch (NullPointerException e) {
+            System.err.println("Erreur: ressource " + imgPath + " introuvable");
+        } catch (Exception e) {
+            System.err.println("ERREUR: impossible de charger l'image " + nom);
+        }
+        // If loading failed
+        return null;
     }
 }
